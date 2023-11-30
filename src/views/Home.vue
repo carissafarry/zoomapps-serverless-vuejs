4cab2e14c9927d0c2a1474c71e3daa8bbc953219
Carissa Farry <69377985+carissafarry@users.noreply.github.com>
2023-11-30 17:55:40 +0700

    feat: change server port

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,33 @@
 <template>
-    <h2 class='text-center'>Start a Meeting to Use this App</h2>
-  <a href="/api/install">Install the app</a>
-<!--    <TodoList v-else-if='isZoom' />-->
-<!--    <v-btn v-else @click='onClick' color='blue text-white'>Install Todos</v-btn>-->
+  <h2 class='text-center' v-if='isMainClient'>Start a Meeting to Use this App</h2>
+  <div v-else-if='isZoom'> Haloo! </div>
+  <a v-else href="/api/install">Install the app</a>
 </template>
 
 <script setup>
-// import TodoList from '../components/Todo/List.vue';
-// import { computed } from 'vue';
-// import zoomSdk from '@zoom/appssdk';
-//
-// import { getApp } from "firebase/app";
-// import { getFunctions, httpsCallable, connectFunctionsEmulator } from 'firebase/functions';
-//
-// const functions = getFunctions(getApp());
-// const install = httpsCallable(functions, 'install');
-//
-// if (process.env.NODE_ENV !== 'production')
-//     connectFunctionsEmulator(functions, 'localhost', 5001)
-//
-// const onClick = async () => {
-//     try {
-//         const {data} = await install()
-//         const installURL = new URL(data)
-//
-//         window.location = installURL.href
-//     } catch (e) {
-//         console.error(e);
-//     }
-// }
-//
-//
-// let context;
-// const isMainClient = computed(() =>
-//     context === 'inMainClient',
-// );
-//
-//
-// let isZoom = false
-//
-// try {
-//     const configResponse = await zoomSdk.config({
-//         size: { width: 480, height: 360 },
-//         capabilities: [
-//             'getMeetingUUID',
-//         ],
-//     });
-//
-//     isZoom = true;
-//
-//     context = configResponse.runningContext;
-//     console.debug('Zoom JS SDK Configuration', configResponse);
-// } catch (e) {
-//     console.error(e);
-// }
+import { computed } from 'vue';
+import zoomSdk from '@zoom/appssdk';
+
+let context;
+const isMainClient = computed(() =>
+    context === 'inMainClient',
+);
+
+let isZoom = false
+
+try {
+    const configResponse = await zoomSdk.config({
+        size: { width: 480, height: 360 },
+        capabilities: [
+            'getMeetingUUID',
+        ],
+    });
+
+    isZoom = true;
+
+    context = configResponse.runningContext;
+    console.debug('Zoom JS SDK Configuration', configResponse);
+} catch (e) {
+    console.error(e);
+}
 </script>
--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
             //     target: 'http://localhost:5001',
             // },
             "/api": {
-                target: 'http://localhost:5173',
+                target: 'http://localhost:3000',
                 changeOrigin: true,
                 secure: false,
             },
